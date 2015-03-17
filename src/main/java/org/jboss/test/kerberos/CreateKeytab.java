147459a590ba3af38a2eed18127b3e983557122d
Ondrej Lukas <olukas@redhat.com>
2015-03-17 09:13:30 +0100

    Added generating keytab for more principals

--- src/main/java/org/jboss/test/kerberos/CreateKeytab.java
+++ src/main/java/org/jboss/test/kerberos/CreateKeytab.java
@@ -50,15 +50,22 @@ public class CreateKeytab {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        if (args == null || args.length != 3) {
+        if (args == null || args.length < 3 || args.length % 2 != 1) {
             System.out.println("Kerberos keytab generator");
             System.out.println("-------------------------");
             System.out.println("Usage:");
             System.out.println("java -classpath kerberos-using-apacheds.jar " + CreateKeytab.class.getName()
-                    + " <principalName> <passPhrase> <outputKeytabFile>");
+                    + " <principalName> <passPhrase> [<principalName2> <passPhrase2> ...] <outputKeytabFile>");
         } else {
-            final File keytabFile = new File(args[2]);
-            createKeytab(args[0], args[1], keytabFile);
+            final File keytabFile = new File(args[args.length - 1]);
+            final List<KeytabPrincipalPassphrase> keytabUsers = new ArrayList<KeytabPrincipalPassphrase>();
+            for (int i = 0; i < args.length - 1; i += 2) {
+                String principal = args[i];
+                String passphrase = args[i + 1];
+                keytabUsers.add(new KeytabPrincipalPassphrase(principal, passphrase));
+                System.out.println("Adding principal " + principal + " with passphrase " + passphrase);
+            }
+            createKeytab(keytabUsers, keytabFile);
             System.out.println("Keytab file was created: " + keytabFile.getAbsolutePath());
         }
     }
@@ -73,18 +80,36 @@ public class CreateKeytab {
      */
     public static void createKeytab(final String principalName, final String passPhrase, final File keytabFile)
             throws IOException {
+        final List<KeytabPrincipalPassphrase> users = new ArrayList<KeytabPrincipalPassphrase>();
+        users.add(new KeytabPrincipalPassphrase(principalName, passPhrase));
+        createKeytab(users, keytabFile);
+    }
+
+    /**
+     * Creates a keytab file for given principals.
+     *
+     * @param keytabUsers
+     * @param keytabFile
+     * @throws IOException
+     */
+    public static void createKeytab(final List<KeytabPrincipalPassphrase> keytabUsers, final File keytabFile)
+            throws IOException {
         final KerberosTime timeStamp = new KerberosTime();
         final int principalType = 1; // KRB5_NT_PRINCIPAL
 
         final Keytab keytab = Keytab.getInstance();
         final List<KeytabEntry> entries = new ArrayList<KeytabEntry>();
-        for (Map.Entry<EncryptionType, EncryptionKey> keyEntry : KerberosKeyFactory.getKerberosKeys(principalName, passPhrase)
-                .entrySet()) {
-            final EncryptionKey key = keyEntry.getValue();
-            final byte keyVersion = (byte) key.getKeyVersion();
-            entries.add(new KeytabEntry(principalName, principalType, timeStamp, keyVersion, key));
+
+        for (KeytabPrincipalPassphrase keytabUser : keytabUsers) {
+            for (Map.Entry<EncryptionType, EncryptionKey> keyEntry : KerberosKeyFactory.getKerberosKeys(
+                    keytabUser.getPrincipalName(), keytabUser.getPassPhrase()).entrySet()) {
+                final EncryptionKey key = keyEntry.getValue();
+                final byte keyVersion = (byte) key.getKeyVersion();
+                entries.add(new KeytabEntry(keytabUser.getPrincipalName(), principalType, timeStamp, keyVersion, key));
+            }
         }
         keytab.setEntries(entries);
         keytab.write(keytabFile);
     }
+
 }
